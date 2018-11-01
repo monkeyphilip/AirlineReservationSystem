768e9fd91caac44573f4bc95d914ae18a9dcc4bd
Zair <Zair@DESKTOP-1U9BID1.gsu.edu>
2018-10-31 20:23:35 -0400

    Added comments confirming the new 'Message' class

--- AirlineReservationSystem/src/TestPackage/Message.java
+++ AirlineReservationSystem/src/TestPackage/Message.java
@@ -3,5 +3,7 @@ package TestPackage;
 public class Message {
 	
 	//This is a message class, summit comments that you want here
+	// Yeah it's unlikely that we'd be able to work today because of Halloween
+	// I have a lot of people I need to help around my neighborhood haha
 
 }
