ec6ec1a81733f500a811bf733f8726252a3cbd8a
Byung Lee <monkey.philip32@gmail.com>
2018-11-03 20:13:20 -0400

    a

--- AirlineReservationSystem/src/TestPackage/Message.java
+++ AirlineReservationSystem/src/TestPackage/Message.java
@@ -5,7 +5,7 @@ public class Message {
 	//This is a message class, summit comments that you want here
 	
 	/*Made User and Flights class under Objects class. 
-	 *Made private properties and getters and setters  // from Philip //
+	 *Made private properties and getters and setters  //from Philip//
 	 */
 
 }
