a5fe6c6a68f7bf8d97dbd076a29bbc01b9b46c3a
Byung Lee <monkey.philip32@gmail.com>
2018-11-03 20:49:29 -0400

    Made City class with properties and setter and getters

---
+++ AirlineReservationSystem/src/Objects/City.java
@@ -0,0 +1,33 @@
+package Objects;
+
+public class City {
+	
+	private String cityName;
+	private String location;
+	private String cityID;
+	
+	
+	
+	
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getCityID() {
+		return cityID;
+	}
+	public void setCityID(String cityID) {
+		this.cityID = cityID;
+	}
+	
+	
+
+}
