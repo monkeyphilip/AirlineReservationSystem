643145e120b28b2b471b9fe6e5a4672c34db7ab0
Byung Lee <monkey.philip32@gmail.com>
2018-12-01 18:16:40 -0500

    same thing

--- AirlineReservationSystem/src/Gui/MainPage.java
+++ AirlineReservationSystem/src/Gui/MainPage.java
@@ -1,5 +1,41 @@
 package Gui;
 
-public class MainPage {
+import java.sql.*;
+
+import Objects.Flights;
+import javafx.application.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.*;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.stage.*;
+
+
+
+public class MainPage extends Application implements EventHandler<ActionEvent> {
+	
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+		
+		
+		
+		
+		
+	}
+	
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
 
 }
