17829653e2d8f9bfb4a54af42a032a7813602aeb
Byung Lee <monkey.philip32@gmail.com>
2018-12-03 03:47:45 -0500

    Made the search poge

--- AirlineReservationSystem/src/Gui/Login.java
+++ AirlineReservationSystem/src/Gui/Login.java
@@ -6,7 +6,6 @@ import javafx.event.*;
 import javafx.geometry.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.text.*;
 import javafx.stage.*;
@@ -39,12 +38,6 @@ public class Login extends Application implements EventHandler<ActionEvent>{
 	}
 
 	@Override
-	public void handle(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		
@@ -64,33 +57,99 @@ public class Login extends Application implements EventHandler<ActionEvent>{
 		loginLabel.setFont(new Font(22.0));
 		
 		Label usernameLabel = new Label("Username:");
-		usernameLabel.setLayoutX(189.0);
+		usernameLabel.setLayoutX(267.0);
 		usernameLabel.setLayoutY(131.0);
 		usernameLabel.setText("Username:");
 		usernameLabel.setFont(new Font(20.0));
 		
 		TextField userTxt = new TextField();
-		userTxt.setLayoutX(311.0);
+		userTxt.setLayoutX(370.0);
 		userTxt.setLayoutY(133.0);
 		userTxt.setPromptText("Username");
 		
 		Label passwordLabel = new Label("Password:");
-		passwordLabel.setLayoutX(193.0);
+		passwordLabel.setLayoutX(270.0);
 		passwordLabel.setLayoutY(174.0);
 		passwordLabel.setFont(new Font(20.0));
 
 		PasswordField passwordTxt = new PasswordField();
-		passwordTxt.setLayoutX(311.0);
+		passwordTxt.setLayoutX(370.0);
 		passwordTxt.setLayoutY(177.0);
 		passwordTxt.setPromptText("Password");
+		passwordTxt.setOnAction(e -> {
+			try {
+				
+			}
+			catch (Exception ex) {
+				
+			}
+		});
 		
 		
+		Button login = new Button("Log In");
+		login.setLayoutX(370.0);
+		login.setLayoutY(222.0);
+		login.setMnemonicParsing(false);
+		login.setPrefHeight(25.0);
+		login.setPrefWidth(149.0);
+		login.setText("Log In");
+		
+		login.setOnAction(e -> {
+			try {
+				
+			}
+			catch(Exception ex) {
+				
+			}
+		});
+		
+		Button register = new Button("Register");
+		register.setLayoutX(370.0);
+		register.setLayoutY(255.0);
+		register.setMnemonicParsing(false);
+		register.setPrefHeight(25.0);
+		register.setPrefWidth(149.0);
+		register.setOnAction(e -> {
+			try {
+				
+			}
+			catch(Exception ex) {
+				
+			}
+		});
+		
+		Button exit = new Button("Exit");
+		exit.setLayoutX(370.0);
+		exit.setLayoutY(328.0);
+		exit.setMnemonicParsing(false);
+		exit.setPrefHeight(25.0);
+		exit.setPrefWidth(150.0);
+		exit.setOnAction(e -> {
+			System.exit(0);
+		});
+		
+		exit.setMinWidth(150);
+		register.setMinWidth(150);
+		login.setMinWidth(150);
+		
+		anchor.getChildren().addAll(userTxt, passwordTxt, login, register, usernameLabel, exit,
+				loginLabel, passwordLabel);
+		scene = new Scene(anchor, 825, 500);
+		;
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.centerOnScreen();
+
 
 
 	}
 	
 	
-	
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	
 	
--- AirlineReservationSystem/src/Gui/MainPage.java
+++ AirlineReservationSystem/src/Gui/MainPage.java
@@ -60,7 +60,26 @@ public class MainPage extends Application implements EventHandler<ActionEvent> {
 		userID.setContentDisplay(javafx.scene.control.ContentDisplay.CENTER);
 		userID.setLayoutX(970.0);
 		userID.setLayoutY(20.0);
-		userID.setText("Logged in as: " + );
+		userID.setText("Logged in as: " + Login.getUser() );
+		userID.setTextAlignment(javafx.scene.text.TextAlignment.RIGHT);
+		userID.setFont(new Font(18.0));
+		
+		searchFlights.setLayoutX(1100.0);
+		searchFlights.setLayoutY(214.0);
+		searchFlights.setMnemonicParsing(false);
+		searchFlights.setText("Search Flights");
+		searchFlights.setOnAction(e -> {
+			SearchPage search = new SearchPage();
+			try {
+				search.start(primaryStage);
+				
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		});
+		
 		
 		
 	}
